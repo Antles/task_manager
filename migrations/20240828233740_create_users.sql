60709e0f0ba14c9735a6da919c26e0a7629b3348
Austin Antles <aantles22@gmail.com>
2024-08-30 10:42:15 -0700

    Change migration order

--- migrations/20240828233740_create_users.sql
+++ migrations/20240828233740_create_users.sql
@@ -1,13 +1,6 @@
-CREATE TABLE tasks
+CREATE TABLE users
 (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  completed BOOLEAN NOT NULL DEFAULT FALSE,
-  category VARCHAR(50) NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  user_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+  username VARCHAR(50) UNIQUE NOT NULL,
+  password_hash VARCHAR(255) NOT NULL
 );
-
-CREATE INDEX idx_tasks_user_id ON tasks(user_id);
